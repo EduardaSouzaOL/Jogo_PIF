0d370327ada157d29da9bfc05d43e56bd72f0f61
EduardaSouzaOL <messtudies18@gmail.com>
2025-11-15 14:50:25 -0300

    configurações do jogo

---
+++ src/config.h
@@ -0,0 +1,27 @@
+
+// config.h - Configurações do jogo
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 600
+
+#define GRAVITY 1500.0f
+#define JUMP_VELOCITY 650.0f
+#define MOVE_SPEED 300.0f
+
+#define MAX_PLATFORMS 64
+#define MAX_ENEMIES 32
+
+#define LEVEL_WIDTH 3000.0f
+#define LEVEL_HEIGHT 1200.0f
+
+#define PLAYER_WIDTH 28.0f
+#define PLAYER_HEIGHT 40.0f
+
+#define ENEMY_WIDTH  28.0f
+#define ENEMY_HEIGHT 28.0f
+
+#define MAX_LIVES 3
+
+#endif
