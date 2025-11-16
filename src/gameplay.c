49a8ee60fad4cd6ac047e696fcac2a5e8d7235f2
EduardaSouzaOL <messtudies18@gmail.com>
2025-11-16 19:09:47 -0300

    external raylib

--- src/gameplay.c
+++ src/gameplay.c
@@ -7,3 +7,13 @@ static Player jogador; // jogador principal, saiu de uma estrutura definida em g
 static Nivel nivelAtual; // nivel atual do jogo, guarda as plataformas e o fundo
 static Camera2D camera; // camera do jogo, visão do jogador
 static float gravidade = 0.45f;     // força da gravidade plmds
+
+// funções privadas, apenas nesse arquivo
+static void InitJogador(void);
+static void UpdateFisica(float dt);
+static void CheckColisoes(void);
+
+// funções públicas, visiveis em gameplay.h
+void GameplayInit(void){
+    
+}
